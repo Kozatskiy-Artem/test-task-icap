598aae034e11d6949849213c37cb39220229b2e4
Artem Kozatskyi <artemfury@gmail.com>
2023-10-03 07:15:27 +0300

    Created partial product update endpoint

--- products/views.py
+++ products/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 from core.containers import ServiceContainer
 from core.permissions import JWTPermissionValidator
 from core.exceptions import InstanceDoesNotExistError
-from .dto import NewProductDTO
-from .serializers import ProductCreateSerializer, ProductSerializer
+from .dto import NewProductDTO, PartialProductDTO
+from .serializers import ProductCreateSerializer, ProductSerializer, PartialProductSerializer
 
 
 class ApiProductListView(APIView):
@@ -49,6 +49,32 @@ class ApiProductDetailView(APIView):
 
         try:
             product_dto = product_service.get_product(id)
+        except InstanceDoesNotExistError as exception:
+            return Response({"error": str(exception.message)}, status=status.HTTP_404_NOT_FOUND)
+
+        product = ProductSerializer(product_dto)
+
+        return Response(
+            data=product.data,
+            status=status.HTTP_200_OK,
+        )
+
+    def patch(self, request, id):
+        """Handle PATCH request to partial update product data."""
+
+        JWTPermissionValidator.is_superuser_or_raise(request)
+
+        product_serializer = PartialProductSerializer(data=request.data)
+
+        if not product_serializer.is_valid():
+            return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        product_service = ServiceContainer.product_service()
+
+        partial_product_dto = PartialProductDTO(**product_serializer.validated_data)
+
+        try:
+            product_dto = product_service.partial_update_product(id, partial_product_dto)
         except InstanceDoesNotExistError as exception:
             return Response({"error": str(exception.message)}, status=status.HTTP_404_NOT_FOUND)
 
